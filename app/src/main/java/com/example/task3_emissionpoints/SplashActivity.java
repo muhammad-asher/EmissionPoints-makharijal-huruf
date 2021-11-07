18b8db44ada706e4a187287adc1724d40b918504
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-11-07 14:59:38 +0500

    Splash Screen Done

---
+++ app/src/main/java/com/example/task3_emissionpoints/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.task3_emissionpoints;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+Handler h=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+h.postDelayed(new Runnable() {
+    @Override
+    public void run() {
+        Intent i=new Intent(SplashActivity.this,ProfileActivity.class);
+        startActivity(i);
+        finish();
+    }
+},1500);
+
+
+
+    }
+}
