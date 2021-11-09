6be15eb37c39a77ae71930a59da7b4aa2da7bb06
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-11-09 21:13:06 +0500

    Complete QuizStartActivity

---
+++ app/src/main/java/com/example/task3_emissionpoints/QuizStartActivity.java
@@ -0,0 +1,30 @@
+package com.example.task3_emissionpoints;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class QuizStartActivity extends AppCompatActivity {
+
+
+    Button btnQuizstart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_start);
+
+        btnQuizstart=(Button) findViewById(R.id.btnStartQuiz);
+
+        btnQuizstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(QuizStartActivity.this, QuizExamActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
